932fd89fdbed41dee324938a9d3caefdef259940
Jan van Brügge <supermanitu@gmail.com>
2018-04-13 14:44:52 +0200

    Update documentation

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,3 +1,249 @@
-﻿var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module","64":"Function","256":"Interface","1024":"Property","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":1,"name":"\"modalify\"","url":"modules/_modalify_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":256,"name":"Open","url":"interfaces/_modalify_.open.html","classes":"tsd-kind-interface tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":2,"kind":1024,"name":"type","url":"interfaces/_modalify_.open.html#type","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Open"},{"id":3,"kind":1024,"name":"component","url":"interfaces/_modalify_.open.html#component","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Open"},{"id":4,"kind":256,"name":"Close","url":"interfaces/_modalify_.close.html","classes":"tsd-kind-interface tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":5,"kind":1024,"name":"type","url":"interfaces/_modalify_.close.html#type","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Close"},{"id":6,"kind":1024,"name":"count","url":"interfaces/_modalify_.close.html#count","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Close"},{"id":7,"kind":256,"name":"Message","url":"interfaces/_modalify_.message.html","classes":"tsd-kind-interface tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":8,"kind":1024,"name":"type","url":"interfaces/_modalify_.message.html#type","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Message"},{"id":9,"kind":1024,"name":"payload","url":"interfaces/_modalify_.message.html#payload","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Message"},{"id":10,"kind":256,"name":"Options","url":"interfaces/_modalify_.options.html","classes":"tsd-kind-interface tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":11,"kind":1024,"name":"name","url":"interfaces/_modalify_.options.html#name","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":12,"kind":1024,"name":"DOMDriverKey","url":"interfaces/_modalify_.options.html#domdriverkey","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":13,"kind":1024,"name":"center","url":"interfaces/_modalify_.options.html#center","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":14,"kind":1024,"name":"modalContainerClass","url":"interfaces/_modalify_.options.html#modalcontainerclass","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":15,"kind":1024,"name":"wrapperClass","url":"interfaces/_modalify_.options.html#wrapperclass","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":16,"kind":1024,"name":"background","url":"interfaces/_modalify_.options.html#background","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":17,"kind":1024,"name":"zIndex","url":"interfaces/_modalify_.options.html#zindex","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"\"modalify\".Options"},{"id":18,"kind":4194304,"name":"Sinks","url":"modules/_modalify_.html#sinks","classes":"tsd-kind-type-alias tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":19,"kind":4194304,"name":"Sources","url":"modules/_modalify_.html#sources","classes":"tsd-kind-type-alias tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":20,"kind":4194304,"name":"Component","url":"modules/_modalify_.html#component","classes":"tsd-kind-type-alias tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":21,"kind":65536,"name":"__type","url":"modules/_modalify_.html#component.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"\"modalify\".Component"},{"id":22,"kind":4194304,"name":"ModalAction","url":"modules/_modalify_.html#modalaction","classes":"tsd-kind-type-alias tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":23,"kind":64,"name":"modalify","url":"modules/_modalify_.html#modalify","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":24,"kind":64,"name":"centerHTML","url":"modules/_modalify_.html#centerhtml","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"modalify\""},{"id":25,"kind":64,"name":"displayModals","url":"modules/_modalify_.html#displaymodals","classes":"tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported","parent":"\"modalify\""},{"id":26,"kind":64,"name":"wrapModals","url":"modules/_modalify_.html#wrapmodals","classes":"tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported","parent":"\"modalify\""},{"id":27,"kind":64,"name":"addStyles","url":"modules/_modalify_.html#addstyles","classes":"tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported","parent":"\"modalify\""}]};
+var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+    kinds: {
+        '1': 'External module',
+        '64': 'Function',
+        '256': 'Interface',
+        '1024': 'Property',
+        '65536': 'Type literal',
+        '4194304': 'Type alias'
+    },
+    rows: [
+        {
+            id: 0,
+            kind: 1,
+            name: '"modalify"',
+            url: 'modules/_modalify_.html',
+            classes: 'tsd-kind-external-module'
+        },
+        {
+            id: 1,
+            kind: 256,
+            name: 'Open',
+            url: 'interfaces/_modalify_.open.html',
+            classes: 'tsd-kind-interface tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 2,
+            kind: 1024,
+            name: 'type',
+            url: 'interfaces/_modalify_.open.html#type',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Open'
+        },
+        {
+            id: 3,
+            kind: 1024,
+            name: 'component',
+            url: 'interfaces/_modalify_.open.html#component',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Open'
+        },
+        {
+            id: 4,
+            kind: 1024,
+            name: 'sources',
+            url: 'interfaces/_modalify_.open.html#sources',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Open'
+        },
+        {
+            id: 5,
+            kind: 256,
+            name: 'Close',
+            url: 'interfaces/_modalify_.close.html',
+            classes: 'tsd-kind-interface tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 6,
+            kind: 1024,
+            name: 'type',
+            url: 'interfaces/_modalify_.close.html#type',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Close'
+        },
+        {
+            id: 7,
+            kind: 1024,
+            name: 'count',
+            url: 'interfaces/_modalify_.close.html#count',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Close'
+        },
+        {
+            id: 8,
+            kind: 256,
+            name: 'Message',
+            url: 'interfaces/_modalify_.message.html',
+            classes: 'tsd-kind-interface tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 9,
+            kind: 1024,
+            name: 'type',
+            url: 'interfaces/_modalify_.message.html#type',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Message'
+        },
+        {
+            id: 10,
+            kind: 1024,
+            name: 'payload',
+            url: 'interfaces/_modalify_.message.html#payload',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Message'
+        },
+        {
+            id: 11,
+            kind: 256,
+            name: 'Options',
+            url: 'interfaces/_modalify_.options.html',
+            classes: 'tsd-kind-interface tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 12,
+            kind: 1024,
+            name: 'name',
+            url: 'interfaces/_modalify_.options.html#name',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 13,
+            kind: 1024,
+            name: 'DOMDriverKey',
+            url: 'interfaces/_modalify_.options.html#domdriverkey',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 14,
+            kind: 1024,
+            name: 'center',
+            url: 'interfaces/_modalify_.options.html#center',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 15,
+            kind: 1024,
+            name: 'modalContainerClass',
+            url: 'interfaces/_modalify_.options.html#modalcontainerclass',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 16,
+            kind: 1024,
+            name: 'wrapperClass',
+            url: 'interfaces/_modalify_.options.html#wrapperclass',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 17,
+            kind: 1024,
+            name: 'background',
+            url: 'interfaces/_modalify_.options.html#background',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 18,
+            kind: 1024,
+            name: 'zIndex',
+            url: 'interfaces/_modalify_.options.html#zindex',
+            classes: 'tsd-kind-property tsd-parent-kind-interface',
+            parent: '"modalify".Options'
+        },
+        {
+            id: 19,
+            kind: 4194304,
+            name: 'Sinks',
+            url: 'modules/_modalify_.html#sinks',
+            classes: 'tsd-kind-type-alias tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 20,
+            kind: 4194304,
+            name: 'Sources',
+            url: 'modules/_modalify_.html#sources',
+            classes: 'tsd-kind-type-alias tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 21,
+            kind: 4194304,
+            name: 'Component',
+            url: 'modules/_modalify_.html#component',
+            classes: 'tsd-kind-type-alias tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 22,
+            kind: 65536,
+            name: '__type',
+            url: 'modules/_modalify_.html#component.__type',
+            classes:
+                'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+            parent: '"modalify".Component'
+        },
+        {
+            id: 23,
+            kind: 4194304,
+            name: 'ModalAction',
+            url: 'modules/_modalify_.html#modalaction',
+            classes: 'tsd-kind-type-alias tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 24,
+            kind: 64,
+            name: 'modalify',
+            url: 'modules/_modalify_.html#modalify',
+            classes: 'tsd-kind-function tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 25,
+            kind: 64,
+            name: 'centerHTML',
+            url: 'modules/_modalify_.html#centerhtml',
+            classes: 'tsd-kind-function tsd-parent-kind-external-module',
+            parent: '"modalify"'
+        },
+        {
+            id: 26,
+            kind: 64,
+            name: 'displayModals',
+            url: 'modules/_modalify_.html#displaymodals',
+            classes:
+                'tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported',
+            parent: '"modalify"'
+        },
+        {
+            id: 27,
+            kind: 64,
+            name: 'wrapModals',
+            url: 'modules/_modalify_.html#wrapmodals',
+            classes:
+                'tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported',
+            parent: '"modalify"'
+        },
+        {
+            id: 28,
+            kind: 64,
+            name: 'addStyles',
+            url: 'modules/_modalify_.html#addstyles',
+            classes:
+                'tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported',
+            parent: '"modalify"'
+        }
+    ]
+};
